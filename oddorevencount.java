321e1e705e0e16a0eca266a5d2ef5e3e37e2a351
tessaangeline <147420296+tessaangeline@users.noreply.github.com>
2024-01-07 11:29:57 +0530

    Update oddorevencount.java

--- oddorevencount.java
+++ oddorevencount.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class question4{
+public class oddorevencount{
     public static void main(String args[]){
         Scanner s= new Scanner(System.in);
         System.out.println("Enter the number of elements in the array: ");
